387ea5801515037b2990a1d492c7fa8c55e571b2
coyrikbae <coyrikbae@gmail.com>
2023-01-29 13:28:37 +0700

    update29012023

---
+++ basic-js/algoritmapseudocode/index.js
@@ -0,0 +1,57 @@
+/**
+ * A. Hitung luas segitiga
+ * ALGORITMA
+ * 1. Rumus luas segitiga 0.5 * alas * tinggi
+ * 2. Buat variabel alas
+ * 3. Buat variabel tinggi
+ * 4. Buat variabel luas
+ * 5. Isi variabel luas dengan rumus 0.5 kali alas kali tinggi
+ * 6. Hitung luas segitigating
+ * 7. Cetak luas segitiga
+ * 
+ * PSEUDOCODE
+ * STORE alas WITH ANY Number
+ * STORE tinggi WITH ANY Number
+ * STORE luas WITH 0.5 MULTIPLY alas MULTIPLY tinggi
+ * DISPLAY luas
+ * 
+ */
+console.log("Hello World!");
+
+let alas = 5;
+let tinggi = 3;
+let x = 0.5;
+let hasilA = x * alas * tinggi;
+console.log("LUAS SEGITIGA " + hasilA);
+
+/**
+ * B. Hitung luas lingkaran
+ * ALGORITMA
+ * Rumus luas lingkaran = 3.14*r*r
+ * 
+ * PSEUDOCODE
+ * DISPLAY hasil luas lingkaran
+ * 
+ */
+var jari = 15;
+var jari2 = 15;
+var hasilB = 22/7 * jari * jari2;
+var text1 = "LUAS LINGKARAN = ";
+console.log(text1, hasilB);
+
+/**
+ * C. Tentukan luas permukaan tabung
+ * ALGORITMA
+ * Rumus luas permukaan tabung = 2*3.14*r*(r+t)
+ * 
+ * PSEUDOCODE
+ * DISPLAY hasil luas permukaan tabung
+ *
+ */
+var a = 2;
+var p = 22 / 7;
+var j1 = 12;
+var j2 = 12;
+var t = 22;
+var hasilC = a * p * j1 * (j2 + t);
+console.log("LUAS PERMUKAAN TABUNG = " + hasilC);
